3df6d9ad9eb07ef07052645ac8b61021bb5ace51
rome <jeromemanciadelossantos@gmail.com>
2025-07-25 10:55:17 +0800

    Updated POS UI Structure

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POS</title>
+
+    <!-- ASSETS -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/pos/pos.css">
+    <title>Point of Sale</title>
 </head>
+
 <body>
-    
+    <div class="root">
+        <div class="sidebar">
+
+        </div>
+        <div class="main">
+            <!-- -->
+            <div class="pos-container">
+                <div class="wrapper">
+                    <div class="category-section">
+
+                    </div>
+                    <div class="product-section">
+
+                    </div>
+                </div>
+                <div class="cart-section">
+                    <div class="cart">
+
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+        </div>
+    </div>
+    <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
